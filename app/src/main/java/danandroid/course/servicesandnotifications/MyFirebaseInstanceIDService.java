b3bed0006cfaebefa6216bb79f16a681f4b53e22
DanielJakar <danieljakar@gmail.com>
2017-07-11 20:23:20 +0300

    Initial commit

---
+++ app/src/main/java/danandroid/course/servicesandnotifications/MyFirebaseInstanceIDService.java
@@ -0,0 +1,30 @@
+package danandroid.course.servicesandnotifications;
+
+import android.content.SharedPreferences;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Get the token for the user in FCM and save it for later usage.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        //can't save to db...no user yet.
+        SharedPreferences prefs = getSharedPreferences("id", MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putString("token", token);
+        //
+        editor.commit();
+
+
+    }
+}
