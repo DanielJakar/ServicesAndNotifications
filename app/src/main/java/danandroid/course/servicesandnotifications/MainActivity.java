5ba707662bf3dc393a70c1706364ba2707e58e2e
DanielJakar <danieljakar@gmail.com>
2017-07-18 18:41:10 +0300

    Added mAuth

--- app/src/main/java/danandroid/course/servicesandnotifications/MainActivity.java
+++ app/src/main/java/danandroid/course/servicesandnotifications/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.firebase.jobdispatcher.Constraint;
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
 import com.firebase.jobdispatcher.GooglePlayDriver;
 import com.firebase.jobdispatcher.Job;
@@ -69,21 +70,71 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, token, Toast.LENGTH_SHORT).show();
         Log.d(TAG, token);
 
+        recurringJob();
 
+    }
+
+    private void oneTimeJobNow (){
         //dispacher dispach and cancel jobs
         FirebaseJobDispatcher dispacher = new FirebaseJobDispatcher(new GooglePlayDriver(this/*context*/));
 
-        int start = (int) java.util.concurrent.TimeUnit.HOURS.toSeconds(2);
-        int end = (int) java.util.concurrent.TimeUnit.HOURS.toSeconds(3);
-
         //criteria for running the job
         Job job = dispacher.newJobBuilder().
-                setTag("my job tag").//tag identifies the job -> cancel
+                setTag("myOneTimeJobNowUniqueTag").//tag identifies the job -> cancel
                 setService(MyJobService.class).
                 setLifetime(Lifetime.UNTIL_NEXT_BOOT).
-                setTrigger(Trigger.executionWindow(start, end)).
+                setTrigger(Trigger.NOW).
                 setRecurring(false).
                 build();
 
+        dispacher.mustSchedule(job);
+        //startService(new Intent(this, myJobService.class);
+    }
+
+    private void oneTimeJobAfterOneHour (){
+        //dispacher dispach and cancel jobs
+        FirebaseJobDispatcher dispacher = new FirebaseJobDispatcher(new GooglePlayDriver(this/*context*/));
+
+        int start = (int) java.util.concurrent.TimeUnit.HOURS.toSeconds(1);
+        int end = (int) java.util.concurrent.TimeUnit.HOURS.toSeconds(2);
+
+        //criteria for running the job
+        Job job = dispacher.newJobBuilder().
+                setTag("myOneTimeJobAfterOneHourUniqueTag").//tag identifies the job -> cancel
+                setService(MyJobService.class).
+                setLifetime(Lifetime.FOREVER). //Boot Complete
+                //Time in seconds
+                setTrigger(Trigger.executionWindow(start, end /*can be start+ 60000...*/)).
+                setRecurring(false).
+                build();
+
+        dispacher.mustSchedule(job);
+        //startService(new Intent(this, myJobService.class);
+    }
+
+    private void recurringJob (){
+        //dispacher dispach and cancel jobs
+        FirebaseJobDispatcher dispacher = new FirebaseJobDispatcher(new GooglePlayDriver(this/*context*/));
+
+        int start = (int) java.util.concurrent.TimeUnit.HOURS.toSeconds(1);
+        int end = 60;
+
+        //criteria for running the job
+        Job job = dispacher.newJobBuilder().
+                setTag("myRecurringUniqueTag").//tag identifies the job -> cancel
+                setService(MyJobService.class).
+                setRecurring(true).
+                setLifetime(Lifetime.FOREVER). //Boot Complete
+                setReplaceCurrent(true).
+                setConstraints(Constraint.ON_ANY_NETWORK).
+                //setConstraints(Constraint.ON_ANY_NETWORK, Constraint.DEVICE_IDLE).
+                //setConstraints(Constraint.ON_UNMETERED_NETWORK, Constraint.DEVICE_CHARGING, Constraint.DEVICE_IDLE).
+                //Time in seconds
+                        //setTrigger(Trigger.executionWindow(start, end /*can be start+ 60...*/)).
+                        setTrigger(Trigger.executionWindow(0, 19)).
+                        build();
+
+        dispacher.mustSchedule(job);
+        //startService(new Intent(this, myJobService.class);
     }
 }
--- app/src/main/java/danandroid/course/servicesandnotifications/MyJobService.java
+++ app/src/main/java/danandroid/course/servicesandnotifications/MyJobService.java
@@ -1,9 +1,19 @@
 package danandroid.course.servicesandnotifications;
 
-import android.util.Log;
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.os.Build;
 
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
+
+import java.util.Date;
 
 /**
  * Created by Jakars on 11/07/2017.
@@ -13,16 +23,92 @@ public class MyJobService extends JobService {
     private static final String TAG = "Ness";
 
     @Override
-    public boolean onStartJob(JobParameters job) {
+    public boolean onStartJob(final JobParameters job) {
         //do the job:
-        showNotification();
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {Thread.sleep(1000);} catch (InterruptedException ignored) { }
+                showNotification();
+                jobFinished(job, true);
+            }
+        });
+        t.start();
 
-        return false; //is there ongoing progress?
+        return true; //is there ongoing work?
     }
 
+
+
+    @TargetApi(Build.VERSION_CODES.O)
     private void showNotification() {
-        Log.d(TAG, "showNotification: ");
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("time", new Date().toString());
+        PendingIntent pi = PendingIntent.getActivity(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+//        Log.d(TAG, "showNotification: ");
+//        android.support.v7.app.NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+//        builder.setContentTitle("Hello, From Recurring service");
+//        builder.setContentText("This is the Text");
+//        builder.setSmallIcon(R.drawable.ic_note);
+//        builder.setChannel("channel1");
+//        //action:
+//        builder.setAutoCancel(true);
+//        builder.setContentIntent(pi);
+//
+//        Notification notification = builder.build();
+//
+//        NotificationManagerCompat.from(this).notify(1, notification);
+
+
+
+        String channelId = "channel2";
+        setupChannel(channelId);
+        Notification.Builder builder = new Notification.Builder(this, channelId);
+        builder.setContentTitle("Hello, From Recurring service");
+        builder.setContentText("This is the Text");
+
+
+        builder.setStyle(new Notification.BigPictureStyle().
+                bigPicture(BitmapFactory.decodeResource(getResources(),R.drawable.worldwidewebmap)));
+
+        builder.setSmallIcon(R.drawable.ic_note);
+       //action:
+        builder.setAutoCancel(true);
+        builder.setContentIntent(pi);
+        Notification notification = builder.build();
+
+        NotificationManager mgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mgr.notify(3,builder.build());
+
     }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void setupChannel (String id){
+        String channelName = getResources().getString(R.string.chanel1_name);
+        NotificationChannel channel = new NotificationChannel(id,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH);
+
+        channel.setDescription(getResources().getString(R.string.channel_description));
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        channel.enableVibration(true);
+        channel.enableLights(true);
+        channel.setSound(RingtoneManager.getDefaultUri(
+                RingtoneManager.TYPE_NOTIFICATION),
+                Notification.AUDIO_ATTRIBUTES_DEFAULT);
+
+        channel.setShowBadge(true);
+
+        //TODO: custom sounds
+        NotificationManager mgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mgr.createNotificationChannel(channel);
+    }
+
+    private void dostuff(){
+
+    }
+
 
     @Override
     public boolean onStopJob(JobParameters job) {
